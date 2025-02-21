914ae5aa9819609a672285c1a54c0942eac3016d
U-BSD\476275 <476275@SHS-B4W2NW3.beaverton.k12.or.us>
2025-02-21 15:29:35 -0800

    Working on shunting still

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <sstream>
-#include <vector> 
+#include <vector>
+#include <cstring>
 #include "stack_and_queue.h"
 using namespace std;
 
@@ -10,9 +11,14 @@ using namespace std;
 //Note: Must output expression tree for infix, postfix, and prefix
 
 void shuntingYard(vector<string> tokens);
+bool isNum(string token);
+bool isOperator(string token);
 
 int main() {
+  cout << isNum("9232"); //returns true (for testing)
+  cout << isOperator("^"); //returns true
 
+  
   string input;
   vector<string> tokens;
   string token;
@@ -39,5 +45,39 @@ int main() {
 void shuntingYard(vector<string> tokens) {
   for (auto & token: tokens) { //reading the tokens
     //check if num, operator or parenthhese
+    if (isNum(token)) {
+
+    } else if (isOperator(token)) {
+
+
+    }
   }
+}
+
+
+//Checks if string is a integer
+bool isNum(string token) {
+  for (char ch : token) {
+    int asciiVal = ch; //convert to ascii value
+    if (!(ch >= 48 && ch <= 57)) { //check if it's in the range of ascii digits
+      return false; //returns false if we find something not in the digit range
+    }
+  }
+  return true;
+}
+
+bool isOperator(string token) {
+  if (token.length() > 1) { //operators should only be one character long
+    cout << "Error: Non number string contains length greater than two. Did you remember to put spaces between everything?" << endl;
+    return false;
+  }
+  
+  string operators("+-*/^");
+
+  for (char ch : operators) {
+    if (token[0] == ch) { //since it should be a single char string only compare the 0 slot
+      return true;
+    }
+  }
+  return false;
 }
